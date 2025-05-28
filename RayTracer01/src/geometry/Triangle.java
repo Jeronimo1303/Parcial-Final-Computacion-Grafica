df09b25175a2e2ebe5f49615761f19eb31ec57a1
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-05-28 11:21:53 -0500

    Created triangle file

---
+++ RayTracer01/src/geometry/Triangle.java
@@ -0,0 +1,16 @@
+package geometry;
+
+public class Triangle implements IntersectableObject {
+    @Override
+    public Solution intersect(Ray ray) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setCamera() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
